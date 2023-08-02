957c4ccbeecd27207c7ffa9975d932e85bc21709
jii <jii@iium.kr>
2023-08-02 15:38:44 +0900

    온라인 등록 작업 진행중

---
+++ sub/_registration.php
@@ -0,0 +1,70 @@
+<?php 
+include_once(EUM_INCLUDE_PATH.'/sub_top.php');
+?>
+
+<div id="registration" class="contents_wrap sub">
+  <?php sub_top($sb_menus, 'introduce', 'aboutus'); ?>
+
+  <div class="sub_contents">
+    
+    <!-- Sub Title { -->
+    <div class="sub_title_box">
+      <div class="wrapper">
+        <p class="sub_title">
+          진행 예정인 세미나를 온라인을 통해 <br>
+          <span class="bold">간편하게</span> 신청하세요.
+        </p>
+      </div>
+    </div>
+    <!-- } Sub Title -->
+
+    <div class="regi_expl_wrap">
+      <div class="wrapper">
+        <div class="regi_expl_box">
+          <p class="expl01">한국신학대학도서관협의회 50주년 기념 세미나</p>
+          <p class="expl02">한국신학대학도서돤협의회 50주년 기념 세미나의 온라인 신청기한은 2023년 7월 7일(금)까지이며, 참가자 명단은 홈페이지에서 확인 가능합니다. <br>많은 관심과 참여를 부탁드립니다.</p>
+          <ul class="i-col-0 regi_expl_info_ul">
+            <li>
+              <p class="title">간사</p>
+              <p class="content">김아라</p>
+            </li>
+            <li>
+              <p class="title">문의</p>
+              <p class="content">042-828-3245/3242</p>
+            </li>
+            <li>
+              <p class="title">이메일</p>
+              <p class="content">ktla1973@naver.com</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="regi_form_wrap">
+      <div class="wrapper">
+        <form action="">
+          <div class="i-col-2 regi_form_list">
+            <div class="regi_form_li">
+              <div class="regi_form_li_inner">
+                <p class="regi_form_title">교육명</p>
+                <div class="regi_form_content">
+                  한국신학대학도서관협의회 50주년 기념 세미나
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="btn_wrap">
+      <div class="wrapper">
+        <div class="btn_box">
+          <a href="" class="btn">온라인 등록</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
