c2c828ed76d92b45847bb3cc3209a671e0cbb1f9
jii <jii@iium.kr>
2023-08-02 14:52:57 +0900

    참가안내 퍼블리싱

---
+++ sub/_participation.php
@@ -0,0 +1,62 @@
+<?php 
+include_once(EUM_INCLUDE_PATH.'/sub_top.php');
+?>
+
+<div id="participation" class="contents_wrap sub">
+  <?php sub_top($sb_menus, 'introduce', 'aboutus'); ?>
+
+  <div class="sub_contents">
+    
+    <!-- Sub Title { -->
+    <div class="sub_title_box">
+      <div class="wrapper">
+        <p class="sub_title">
+          한국신학대학도서관협의회는 세미나를 통해 <br>
+          <span class="bold">신학계 도서관의 위상</span>을 높이고 있습니다.
+        </p>
+      </div>
+    </div>
+    <!-- } Sub Title -->
+
+    <div class="participation_method_wrap">
+      <div class="wrapper">
+        <ul class="participation_method_list">
+          <li>
+            <p class="participation_method_title">참가 신청 방법</p>
+            <div class="participation_method_content">
+              <p class="participation_method_item">한국신학도서관협의회 홈페이지(www.ktla.or.kr)에서 [메인] 페이지 > [세미나 안내] 페이지에서 신청 가능합니다.</p>
+              <p class="participation_method_item">2013년부터 온라인을 통해 한국신학도서관협의회 세미나 등록 및 확인이 가능합니다.</p>
+              <p class="participation_method_item">참가신청 및 입금이 완료되면 홈페이지에서 참가증과 영수증을 출력할 수 있습니다.</p>
+            </div>
+          </li>
+          <li>
+            <p class="participation_method_title">참가 신청시<br> 주의사항</p>
+            <div class="participation_method_content">
+              <p class="participation_method_item">한국신학도서관협의회 홈페이지(www.ktla.or.kr)에서 [메인] 페이지 > [세미나 안내] 페이지에서 신청 가능합니다.</p>
+              <p class="participation_method_item">참가신청접수 여부 등 관련 소식을 SMS(문자메세지) 또는이메일로 보내드립니다.</p>
+              <p class="participation_method_item">참가신청이 완료되면 [세미나 안내] > [등록확인 및 수정]페이지에서 확인하실 수 있습니다.</p>
+            </div>
+          </li>
+          <li>
+            <p class="participation_method_title">문의처</p>
+            <div class="participation_method_content">
+              <p class="participation_method_item">한국신학대학도서관협의회 사무국</p>
+              <p class="participation_method_item">전화 : 042-828-3245 / 3242</p>
+              <p class="participation_method_item">팩스 : 042-828-3239</p>
+              <p class="participation_method_item">이메일 : ktla1973@naver.com</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="btn_wrap">
+      <div class="wrapper">
+        <div class="btn_box">
+          <a href="" class="btn">온라인 등록</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
