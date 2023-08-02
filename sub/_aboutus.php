f8af23eb411590fe351b0d122b4608c4679e956a
onesick <onesick@iium.kr>
2023-08-02 16:04:16 +0900

    신대도협 소개 > 협회안내 PC 완료

--- sub/_aboutus.php
+++ sub/_aboutus.php
@@ -6,6 +6,70 @@ include_once(EUM_INCLUDE_PATH.'/sub_top.php');
   <?php sub_top($sb_menus, 'introduce', 'aboutus'); ?>
 
   <div class="sub_contents">
-    <h2 style="padding: 300px 0; background: #f4f4f4;">dgf</h2>
+    <div class="aboutus-banner">
+      <div class="aboutus-banner-img"></div>
+    </div>
+    
+    <div class="sub_title_box">
+      <div class="container">
+        <div class="wrapper">
+          <p class="sub_title">
+            신학도서관의 발전과 함께 해온 50주년의 역사, <br>
+            <span class="bold">한국신학대학도서관협의회</span>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="aboutus-text_box">
+      <div class="container">
+        <div class="wrapper">
+          <p class="aboutus-text">
+            한국신학대학도서관협의회는 전국의 신학도서관 발전을 도모하며 신학도서관의 운영 및 회원교(기관)의 유대 강화와 협력 증진에 이바지함을 목적으로 한다. 1973년 4월 16일 13개교를
+            중심으로 발족하였으며, 현재까지 회원교 49개교가 가입하여 50주년의 역사를 기록하고 있다.<br/>
+            협의회 발족 이전에는 신학계 대학 사서들이 각자 도서관을 운영, 발전시켜왔으나 자체적인 한계를 느끼면서 대학 간의 협력과 표준화된 업무처리의 필요성을 절감해 오던 중에 1972년
+            6월 1일 미국남침례회 신학교 도서관장 Dr. Leo Crismon의 한국 방문이 계기가 되어 전국의 신학도서관장과 사서들이 모여 도서관 운영의 발전을 위해서는 도서관 연합운동이 필요하다
+            는 의견에 합의하였다.<br/>
+            그 후 5차에 걸쳐 걸친 준비 모임의 결과로 정관의 초안이 작성되고 협의회 명칭도 한국신학도서관협의회로 정해져 1973년 4월에 본회가 창립된 것이다. 창립 이래 매년 2회 이상의 세미
+            나와 신학 석사학위논문 CD-ROM 제작, 신학저널의 컨소시엄사업 구축, 신학 분야의 전자책 제작 사업 및 교회도서관 워크숍 개최 등 지속해서 실시함으로써 신학계 도서관의 위상을 높
+            이고 있다.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="aboutus-summary">
+      <div class="container">
+        <div class="wrapper">
+          <div class="match-height inner">
+            <div class="aboutus-summary-list">
+              <div class="icon">
+                <img src="/ktla-dev/source/img/aboutus-summary1-icon.png" alt="">
+              </div>
+              <div class="text">
+                <p class="title">창립</p>
+                <p class="expl">1973년 4월 16일</p>
+              </div>
+            </div>
+            <div class="aboutus-summary-list">
+              <div class="icon">
+                <img src="/ktla-dev/source/img/aboutus-summary2-icon.png" alt="">
+              </div>
+              <div class="text">
+                <p class="title">목적</p>
+                <p class="expl">한국신학도서관의 발전을 도모하며 신학 도서관의 운영 및 회원교(기관)의 유대 강화와 협력증진에 기여함</p>
+              </div>
+            </div>
+            <div class="aboutus-summary-list">
+              <div class="icon">
+                <img src="/ktla-dev/source/img/aboutus-summary3-icon.png" alt="">
+              </div>
+              <div class="text">
+                <p class="title">회원 현황</p>
+                <p class="expl">현 49개교</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
